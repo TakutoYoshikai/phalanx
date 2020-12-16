c96e8e92ff17f54e803dfe76f615f6cc199ed5f7
lucky <takuto.yoshikai@gmail.com>
2020-12-16 15:01:10 +0900

    create a function

--- phalanx/phalanx.py
+++ phalanx/phalanx.py
@@ -1,17 +1,20 @@
 import hashlib
 import argparse
 
+def generate_password(filepath, password):
+    f = open(filepath, "rb")
+    content = f.read()
+    content = bytearray(content)
+    if password != None:
+        content.extend(password.encode())
+    return hashlib.md5(content).hexdigest()
+
 def main():
     parser = argparse.ArgumentParser(description="phalanx is a password generator with any file")
     parser.add_argument("file")
     parser.add_argument("-p", "--password")
     args = parser.parse_args()
-    f = open(args.file, "rb")
-    content = f.read()
-    content = bytearray(content)
-    if args.password != None:
-        content.extend(args.password.encode())
-    print(hashlib.md5(content).hexdigest())
+    print(generate_password(args.file, args.password))
 
 if __name__ == "__main__":
     main()
